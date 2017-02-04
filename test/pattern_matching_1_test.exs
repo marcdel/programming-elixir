0a505a76996cac437b5db20a0a38a63efbe313cf
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-02-04 14:50:31 -0800

    Add pattern matching 1 tests.

--- test/pattern_matching_1_test.exs
+++ test/pattern_matching_1_test.exs
@@ -1,10 +1,41 @@
-ExUnit.start
-
 defmodule PatternMatchingTest do
   use ExUnit.Case
   
   test "a = [1,2,3]" do
     a = [1,2,3]
     assert a == [1,2,3]
+  end
+
+  test "a = 4" do
+    a = 4
+    assert a == 4
+  end
+
+  test "4 = a" do
+    a = 4
+    4 = a
+    assert a == 4
+  end
+
+  test "[a,b] = [1,2,3]" do
+    assert_raise MatchError, fn ->
+      [a,b] = [1,2,3]
+    end
+  end
+
+  test "a = [[1,2,3]]" do
+    a = [[1,2,3]]
+    assert a == [[1,2,3]]
+  end
+
+  test "[a] = [[1,2,3]]" do
+    [a] = [[1,2,3]]
+    assert a == [1,2,3]
+  end
+
+  test "[[a]] = [[1,2,3]]" do
+    assert_raise MatchError, fn ->
+      [[a]] = [[1,2,3]]
+    end
   end
 end
--- test/programming_elixir_test.exs
+++ test/programming_elixir_test.exs
@@ -1,8 +1,3 @@
 defmodule ProgrammingElixirTest do
   use ExUnit.Case
-  doctest ProgrammingElixir
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
