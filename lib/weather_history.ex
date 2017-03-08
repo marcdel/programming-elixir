353b2abc30a7c1d55bb89229493e6a47af6e37d6
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-03-07 20:11:52 -0800

    Fix warnings about unused args

--- lib/weather_history.ex
+++ lib/weather_history.ex
@@ -1,7 +1,7 @@
 defmodule WeatherHistory do
-  def for_location([], location), do: []
+  def for_location([], _location), do: []
   def for_location([head = [_, location, _, _] | tail], location) do
     [head | for_location(tail, location)]
   end
-  def for_location([head | tail], location), do: for_location(tail, location)
+  def for_location([_head | tail], location), do: for_location(tail, location)
 end
