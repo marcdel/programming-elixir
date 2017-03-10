b9d710314f97d0aefab7fd53521fe1c436382a65
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-03-09 21:32:20 -0800

    Exploring processes.

---
+++ lib/bette_and_joan.ex
@@ -0,0 +1,27 @@
+defmodule BetteAndJoan do
+  def do_work do
+    receive do
+      name ->
+        IO.inspect {:ok, name}
+    end
+  end
+
+  def run do
+    for _ <- 1..10 do
+      Enum.random([:bette, :joan])
+      |> create_process
+    end
+  end
+
+  def create_process :bette do
+    IO.puts "Spawning #{:bette}"
+    bette = spawn(BetteAndJoan, :do_work, [])
+    send bette, :bette
+  end
+
+  def create_process :joan do
+    IO.puts "Spawning #{:joan}"
+    joan = spawn(BetteAndJoan, :do_work, [])
+    send joan, :joan
+  end
+end
---
+++ lib/chain.ex
@@ -0,0 +1,26 @@
+defmodule Chain do
+  def counter(next_pid) do
+    receive do
+      n ->
+        send next_pid, n + 1
+    end
+  end
+
+  def create_processes(n) do
+    last = Enum.reduce 1..n, self(), fn (_,send_to) ->
+      # Each time we spawn a new process, we pass it the previous process’s PID in the send_to parameter.
+      spawn(Chain, :counter, [send_to])  # returns pid to next iteration
+    end
+
+    send last, 0     # start the count by sending a zero to the last process
+
+    receive do       # and wait for the result to come back to us
+      final_answer when is_integer(final_answer) ->
+        "Result is #{inspect(final_answer)}"
+    end
+  end
+
+  def run(n) do
+    IO.puts inspect :timer.tc(Chain, :create_processes, [n])
+  end
+end
