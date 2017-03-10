a71cd3ed7fb32116572e7b273e25322a6ad6572c
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-03-10 13:30:42 -0800

    By not matching the id, results could be returned out of order.

--- lib/parallel.ex
+++ lib/parallel.ex
@@ -9,4 +9,15 @@ defmodule Parallel do
       receive do { ^pid, result } -> result end
     end)
   end
+
+  def rando_pmap(collection, fun) do
+    me = self
+    collection
+    |> Enum.map(fn (elem) ->
+      spawn_link fn -> (send me, fun.(elem)) end
+    end)
+    |> Enum.map(fn (pid) ->
+      receive do result -> result end
+    end)
+  end
 end
--- test/parallel_test.exs
+++ test/parallel_test.exs
@@ -1,8 +1,27 @@
 defmodule ParallelTest do
   use ExUnit.Case
+  import :timer, only: [ sleep: 1 ]
+
+  # With 50 elements doing a long running taskrando_pmap should always
+  # return at least one element out of order, but pmap should always be correct.
+  @list 1..50
+
+  def slow_work element do
+    sleep 10
+    element * 2
+  end
 
   test "pmap returns mapped elements in correct order" do
-    result = Parallel.pmap 1..5, &(&1 * 2)
-    assert result == [2, 4, 6, 8, 10]
+    expected_result = Enum.map @list, &(slow_work(&1))
+    result = Parallel.pmap @list, &(slow_work(&1))
+
+    assert result == expected_result
+  end
+
+  test "rando_pmap returns mapped elements in any order" do
+    expected_result = Enum.map @list, &(slow_work(&1))
+    result = Parallel.rando_pmap @list, &(slow_work(&1))
+
+    assert result != expected_result
   end
 end
